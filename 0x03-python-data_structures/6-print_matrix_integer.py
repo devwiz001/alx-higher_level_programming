bb6b85f054cbe281aa89fd5345449363203b3948
devwiz001 <wisdomokpara04@gmail.com>
2023-12-04 16:55:49 -0800

    ..

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,3 +8,8 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(num))
 
                 print()
+                
+                # this is an example usage:
+                if __name__ == "__main__":
+                    example_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+                    print_matrix_integer(example_matrix)
