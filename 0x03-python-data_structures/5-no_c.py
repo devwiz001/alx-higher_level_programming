58e19ff65936b57f1c53da913a59488db2fe2a72
devwiz001 <wisdomokpara04@gmail.com>
2023-12-04 16:23:36 -0800

    ..

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for elements in my_string:
-        if elements != "c" and elements != "C":
-            new_string += elements
-            return new_string
+    return ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
+
+# this is an example usage:
+if __name__ == "__main__":
+    input_string = "This is just a Simple Sample String with Cs and Cs"
+    result = no_c(input_string)
+    print(result)
