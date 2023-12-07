3f944ab5b0f85a588a7edd063cb195ce7f5ff545
devwiz001 <wisdomokpara04@gmail.com>
2023-12-06 17:46:32 -0800

    stikk on it

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
