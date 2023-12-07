eed87e3d88b958c1f9ce6567736c053ba29d3df6
devwiz001 <wisdomokpara04@gmail.com>
2023-12-06 16:50:57 -0800

    ..

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for element in my_list:
-        new_list.append(replace if element == search else element)
-        return new_list
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+            return new_list
