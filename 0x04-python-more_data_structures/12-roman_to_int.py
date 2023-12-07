f0dba931fab3dbcf6d9a75c06d6fd3d4e515203c
devwiz001 <wisdomokpara04@gmail.com>
2023-12-06 17:30:55 -0800

    i tried my best

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    result = 0
+    prev_value = 0
+    for char in roman_string:
+        value = roman_map.get(char, 0)
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+            prev_value = value
+            return result
